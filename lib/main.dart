3fc266e7cca9ae3bd0f73e2b7764476296132255
Idemudia <idemudiamondayosaghae@gmail.com>
2023-01-30 09:42:18 +0200

    edit UI title

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter_BLOC Demo'),
+      home: const MyHomePage(title: 'Flutter_BLOC Demo without Library'),
     );
   }
 }
